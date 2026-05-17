c4d0ac5d6aaa39410e707d8501b95687a5a19aa9
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-16 21:28:34 -0300

    chore: update shareCode to WWUPINF24RC

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=HYHYXYZREML&afp=tapi100" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=WWUPINF24RC&afp=tapi100" });
     res.end();
   }
