b4c5f0fe8abc9e7595c67e3e5a1b7b5e9efd6043
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-19 21:45:23 -0300

    chore: update shareCode to SCI7WMFGBU7

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=N8U15OPTAU5&afp=tapi100" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=SCI7WMFGBU7&afp=tapi100" });
     res.end();
   }
