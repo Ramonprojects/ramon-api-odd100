622ea919303611ac0198e20a4a65babdc8cec532
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-25 21:12:39 -0300

    chore: update shareCode to ZMV2DP4P0WH

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=NZN1HZOPNY5&afp=tapi100" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=ZMV2DP4P0WH&afp=tapi100" });
     res.end();
   }
