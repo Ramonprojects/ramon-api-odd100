e4205518910abf59c1a1388874e472e3ccba3f1f
Ramonprojects <adm@ramonmago.com>
2026-05-29 10:57:23 -0300

    chore: shareCode NAP0SIAYUWH -> QM37GAS1NGI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=NAP0SIAYUWH&afp=tapi17" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=QM37GAS1NGI&afp=tapi17" });
     res.end();
   }
