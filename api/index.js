934460b89f925a9a0d98abeee08f8b79e89b9762
Ramonprojects <adm@ramonmago.com>
2026-05-30 21:38:36 -0300

    chore: shareCode QM37GAS1NGI -> W4V3V1QHFBI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=QM37GAS1NGI&afp=tapi100" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=W4V3V1QHFBI&afp=tapi100" });
     res.end();
   }
