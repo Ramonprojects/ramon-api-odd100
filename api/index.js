65ff58067451b4af76be89f772b46d82c27cb61b
Ramonprojects <adm@ramonmago.com>
2026-06-01 22:36:55 -0300

    chore: shareCode W4V3V1QHFBI -> 4KNA9CIRPFK (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=W4V3V1QHFBI&afp=tapi100" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=4KNA9CIRPFK&afp=tapi100" });
     res.end();
   }
