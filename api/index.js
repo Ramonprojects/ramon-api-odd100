02a11dadb520655b3fcf9bc22b6902544c3bb619
Ramonprojects <adm@ramonmago.com>
2026-06-04 22:09:01 -0300

    chore: shareCode IZFS6PTQQZ7 -> DPVI48GV2WQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=IZFS6PTQQZ7&afp=tapi100" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=DPVI48GV2WQ&afp=tapi100" });
     res.end();
   }
