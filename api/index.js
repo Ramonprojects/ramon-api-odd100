6cc289b0abcf0f8e8c3ae5d0ab1e9a0b3e103831
Ramonprojects <adm@ramonmago.com>
2026-06-09 10:05:39 -0300

    chore: shareCode IF1L0D35UIR -> CPFN75EWNI5 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=IF1L0D35UIR&afp=tapi100" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/fowsuqio?shareCode=CPFN75EWNI5&afp=tapi100" });
     res.end();
   }
