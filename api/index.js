ae97cb952a3ee0be9f20a8c63a65bcd0be0adfed
Ramonprojects <adm@ramonmago.com>
2026-06-14 22:33:12 -0300

    chore: shareCode NSQK3LZF2B1 -> HS4MNEMIYGQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737&shareCode=NSQK3LZF2B1&afp2=tapi100" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737&shareCode=HS4MNEMIYGQ&afp2=tapi100" });
     res.end();
   }
