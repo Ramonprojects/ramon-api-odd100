4ef3bdee1b3559fb611eba4733196fd923077153
Ramonprojects <adm@ramonmago.com>
2026-06-16 22:42:34 -0300

    chore: shareCode UUC7RTH1AGQ -> ZQO6SBI0HCS (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737&shareCode=UUC7RTH1AGQ&afp2=tapi100" });
+    res.writeHead(302, { Location: "https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737&shareCode=ZQO6SBI0HCS&afp2=tapi100" });
     res.end();
   }
