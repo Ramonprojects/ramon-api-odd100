bc44744ed104161e82cb207af464b3d4810d9b0c
Ramonprojects <adm@ramonmago.com>
2026-07-01 22:36:23 -0300

    chore: shareCode W34E4542EN1 -> GMTOTNLAGVO (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252'
 
-    const shareCode = '&shareCode=W34E4542EN1'
+    const shareCode = '&shareCode=GMTOTNLAGVO'
     
     const afp = '&afp6=tapi100'
     
