fc946709ceaa82251b6b0a1f5c4d0ace4ebc0df5
Ramonprojects <adm@ramonmago.com>
2026-08-07 20:24:09 -0300

    chore: shareCode 1SLCKCW5IPR -> 6IW52UDJ6ST (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737'
 
-    const shareCode = '&shareCode=1SLCKCW5IPR'
+    const shareCode = '&shareCode=6IW52UDJ6ST'
     
     const afp = '&afp2=tapi100'
     
