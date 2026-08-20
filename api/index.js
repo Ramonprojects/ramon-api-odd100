77e1f176242ebe0fbcba5bbab6bb94695e2a8883
Ramonprojects <adm@ramonmago.com>
2026-08-20 01:30:20 -0300

    chore: shareCode KNU32CHRPAS -> 8LMELN06RVA (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737'
 
-    const shareCode = '&shareCode=KNU32CHRPAS'
+    const shareCode = '&shareCode=8LMELN06RVA'
     
     const afp = '&afp2=tapi100'
     
